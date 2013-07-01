d38d39e685cf6f6c77936ffd61c12ee766dd57e3
Gigo <gigo@aufmbergschneits.net>
2013-07-01 19:10:19 +0200

    Merged FlipdotImage to FlipdotMatrix

--- hqstatus.py
+++ hqstatus.py
@@ -20,7 +20,7 @@ class HqStatusFlipdotAdapter(object):
         self.showStatusText(hqstatus)   
         
     def showStatusText(self, hqstatus):
-        self.__flipdotMatrix.showText(hqstatus)
+        self.__flipdotMatrix.showBlitText(hqstatus)
         
     def run(self):
         while True:
